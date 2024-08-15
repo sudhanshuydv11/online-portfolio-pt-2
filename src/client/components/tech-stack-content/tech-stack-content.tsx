c73839f3a182397da2365ed7739cd090cdf03689
sudhanshuydv11 <73772203+sudhanshuydv11@users.noreply.github.com>
2024-08-15 19:47:17 +0530

    adding more items in preffered tech stack content

--- src/client/components/tech-stack-content/tech-stack-content.tsx
+++ src/client/components/tech-stack-content/tech-stack-content.tsx
@@ -6,6 +6,18 @@ const CONTENT_DATA=[
     {
         imgSrc:'https://th.bing.com/th/id/OIP.K-4RqDC6zFrpAG31ayDDOgHaHa?rs=1&pid=ImgDetMain',
         name:'REACT'
+    },
+    {
+        imgSrc: 'https://miro.medium.com/v2/resize:fit:1358/1*-SEJ0Yo6hJueWc4R-nnU5A.jpeg',
+        name:'REDUX'
+    },
+    {
+        imgSrc:'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        name: 'TypeScript'
+    },
+    {
+        imgSrc:'https://cdn.iconscout.com/icon/free/png-512/free-webpack-logo-icon-download-in-svg-png-gif-file-formats--technology-social-media-vol-7-pack-logos-icons-3030288.png',
+        name: 'Webpack'
     }
 ];
 
