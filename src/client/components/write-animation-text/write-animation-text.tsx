5ed238b155f1d172e4500ad94d1dd3495d0c864d
sudhanshuydv11 <73772203+sudhanshuydv11@users.noreply.github.com>
2024-10-04 11:31:16 +0530

    completing all animations

--- src/client/components/write-animation-text/write-animation-text.tsx
+++ src/client/components/write-animation-text/write-animation-text.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "./write-animation-text-style.scss";
 
+type myStyleType=Record<string, string>;
+
 export const WriteAnimationText=({text, isInterSecting}:{text:string, isInterSecting:boolean })=>{
-    let count=0;
 
     
     return <>
     {<><div>{text.split('')
     .map((character , index)=>{
-        count++;
-    return <span key={`${index}${character}`} className={isInterSecting?'text-animation':'text'} style={{'--delay':`${(count -1) * 0.01}s`}}>
+        const myStyle: myStyleType={'--delay':`${(index) * 0.01}s`};
+    return <span key={`${index}${character}`} className={isInterSecting?'text-animation':'text'} style={myStyle}>
         {character}
     </span>;
     }
