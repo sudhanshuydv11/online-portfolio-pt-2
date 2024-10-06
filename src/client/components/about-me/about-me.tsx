8db285a7b364d259f1cf5e727abfe83bb08d87f1
sudhanshuydv11 <73772203+sudhanshuydv11@users.noreply.github.com>
2024-10-06 15:19:30 +0530

    presentable about me

--- src/client/components/about-me/about-me.tsx
+++ src/client/components/about-me/about-me.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TriggerWhenInView } from "../trigger-when-in-view.tsx";
+import { TriggerWhenInView } from "../trigger-when-in-view/trigger-when-in-view.tsx";
 import './about-me.scss';
 import { WriteAnimationText } from "../write-animation-text/write-animation-text.tsx";
 
@@ -7,26 +7,22 @@ export const AboutMe=()=>{
 
    const aboutMeContent='Currently working on frontend technologies, but dont want to be limited to them. Always looking forward to learning new technologies in different domains. If I could live a thousand years, i would learn everything the world has to offer.';
 
-return <>
-<TriggerWhenInView baseClass="translate-content"
-   intersectionClass='translate-content--animate'
-   style={{}}>
+return <div className="about-me-wrapper">
+<TriggerWhenInView triggerOnce={false}>
 {
-   ()=><div className="about-me">
+   ({isInterSecting})=><div className={`about-me translate-content ${isInterSecting ? 'translate-content--animate':'' }`}>
    ABOUT ME
 </div>
 }
 </TriggerWhenInView>
 
 <div className="about-me-content">
-<TriggerWhenInView baseClass=''
-   intersectionClass=''
-   style={{}}>
+<TriggerWhenInView triggerOnce={false}>
 {
-   ({isInterSecting}: {isInterSecting:boolean})=> <WriteAnimationText text={aboutMeContent} isInterSecting={isInterSecting}/>
+   ({isInterSecting})=> <WriteAnimationText text={aboutMeContent} isInterSecting={isInterSecting}/>
 
 }
 </TriggerWhenInView>
 </div>
-</>;
+</div>;
 };
